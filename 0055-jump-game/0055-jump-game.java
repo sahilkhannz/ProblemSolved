807a183a06ff3b4be4ce544696c484475946f96b
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-04-27 17:37:11 +0530

    Time: 1 ms (99.96%), Space: 45.5 MB (51.94%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean flag=true;
         int n=nums.length,reachable=n-1;
         for(int i=n-1;i>=0;i--){
-            if((nums[i]+i)>=reachable){
-                flag=true;
+            if((nums[i]+i)>=reachable)
                 reachable=i;
-            }else
-                flag=false;
         }
-        return flag;
+        if(reachable==0)
+            return true;
+        return false;
     }
 }
