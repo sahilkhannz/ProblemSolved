d498516e02993af6b5ec60d28f3d6794b2f28ca7
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-05-07 17:04:09 +0530

    Time: 72 ms (19.47%), Space: 61.5 MB (94.61%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -11,34 +11,51 @@
 class Solution {
     
     public ListNode removeNodes(ListNode head) {
-        Stack<Integer> s=new Stack<>();
-        if(head==null)
-            return null;
-        s.push(head.val);
-        head=head.next;
+//         Stack<Integer> s=new Stack<>();
+//         if(head==null)
+//             return null;
+//         s.push(head.val);
+//         head=head.next;
         
-        while(head!=null){
-            if(head.val<s.peek()){
-                s.push(head.val);
-            }else{
-                while(!s.isEmpty() && head.val>s.peek())
-                    s.pop();
-                s.push(head.val);
+//         while(head!=null){
+//             if(head.val<s.peek()){
+//                 s.push(head.val);
+//             }else{
+//                 while(!s.isEmpty() && head.val>s.peek())
+//                     s.pop();
+//                 s.push(head.val);
+//             }
+//             head=head.next;
+//         }
+//         Stack<Integer> tempStack = new Stack<>();
+//     while (!s.isEmpty()) {
+//         tempStack.push(s.pop());
+//     }
+//         ListNode dummy=new ListNode(-1);// Dummy node to simplify insertion
+//         ListNode ans=dummy;
+//         while(!tempStack.isEmpty()){
+//             ListNode x=new ListNode(tempStack.pop());
+//             ans.next=x;
+//             ans=ans.next;
+//         }
+        
+//         return dummy.next;
+        
+        ListNode curr=head;
+        Stack<ListNode> s=new Stack<>();
+        while(curr!=null){
+            while(!s.isEmpty() && curr.val>s.peek().val){
+                s.pop();
             }
-            head=head.next;
+            s.push(curr);
+            curr=curr.next;
         }
-        Stack<Integer> tempStack = new Stack<>();
-    while (!s.isEmpty()) {
-        tempStack.push(s.pop());
-    }
-        ListNode dummy=new ListNode(-1);// Dummy node to simplify insertion
-        ListNode ans=dummy;
-        while(!tempStack.isEmpty()){
-            ListNode x=new ListNode(tempStack.pop());
-            ans.next=x;
-            ans=ans.next;
+        ListNode nxt=curr;
+        while(!s.isEmpty()){
+            curr=s.pop();
+            curr.next=nxt;
+            nxt=curr;
         }
-        
-        return dummy.next;
+    return curr;
     }
 }
