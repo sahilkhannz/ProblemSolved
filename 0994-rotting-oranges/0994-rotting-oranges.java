0ad642af688168b4e2f6bca6931bdc5c6b744dd1
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-05-16 14:54:53 +0530

    Time: 2 ms (72.70%), Space: 42.1 MB (68.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,18 +3,16 @@ class Solution {
         return (x>=0 && y>=0 && x<r && y<c);
     }
     public int orangesRotting(int[][] grid) {
-        int m=grid.length,n=grid[0].length;
+        int freshOrange=0,minute=0,n=grid.length,m=grid[0].length;
         Queue<int []> q=new LinkedList<>();
-        int freshOrange=0,minute=0;
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.add(new int[]{i,j});
                 }
-                if(grid[i][j]==1){
+                if(grid[i][j]==1)
                     freshOrange++;
-                }
+                
             }
         }
         if(freshOrange==0)
@@ -23,40 +21,35 @@ class Solution {
             return -1;
         while(!q.isEmpty()){
             int size=q.size();
-            for(int i=0;i<size;i++){
+            while(size-- !=0){
                 int cell[]=q.poll();
                 int x=cell[0];
                 int y=cell[1];
-                
-                if(safe(x-1,y,m,n) && grid[x-1][y]==1){
-                    grid[x-1][y]=2;
+                if(safe(x-1,y,n,m) && grid[x-1][y]==1){
                     freshOrange--;
+                    grid[x-1][y]=2;
                     q.add(new int[]{x-1,y});
                 }
-                if(safe(x+1,y,m,n) && grid[x+1][y]==1){
-                    grid[x+1][y]=2;
+                if(safe(x+1,y,n,m) && grid[x+1][y]==1){
                     freshOrange--;
+                    grid[x+1][y]=2;
                     q.add(new int[]{x+1,y});
                 }
-                if(safe(x,y-1,m,n) && grid[x][y-1]==1){
-                    grid[x][y-1]=2;
+                if(safe(x,y+1,n,m) && grid[x][y+1]==1){
                     freshOrange--;
-                    q.add(new int[]{x,y-1});
-                }
-                if(safe(x,y+1,m,n) && grid[x][y+1]==1){
                     grid[x][y+1]=2;
-                    freshOrange--;
                     q.add(new int[]{x,y+1});
+                }
+                if(safe(x,y-1,n,m) && grid[x][y-1]==1){
+                    freshOrange--;
+                    grid[x][y-1]=2;
+                    q.add(new int[]{x,y-1});
                 }
             }
             minute++;
         }
-        if(freshOrange==0){
-            //due last rotten oranges 
-            //we make -1 last orange also check after that minute will increase by one
+        if(freshOrange==0)
             return minute-1;
-        }
         return -1;
-        
     }
 }
