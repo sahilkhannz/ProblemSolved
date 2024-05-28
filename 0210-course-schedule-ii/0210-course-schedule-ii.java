de779d8a328bd0ee4129da9864699ee417555fe6
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-05-28 14:19:13 +0530

    Time: 5 ms (75.97%), Space: 45.9 MB (29.90%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,50 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int []topo=new int[numCourses];
+        int []indegree=new int[numCourses];
+        
+        //convert in graph
         List<List<Integer>> graph=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             graph.add(new ArrayList<>());
         }
-        
         for(int i=0;i<prerequisites.length;i++){
+            
             int u=prerequisites[i][0];
             int v=prerequisites[i][1];
             
             graph.get(v).add(u);
         }
-        int ans[]=new int[numCourses];
-        int indegree[]=new int[numCourses];
-        
         for(int i=0;i<numCourses;i++){
-            for(int it:graph.get(i)){
+            for(int it:graph.get(i))
                 indegree[it]++;
-            }
         }
+        
         Queue<Integer> q=new LinkedList<>();
+        // adding those element whose indegree is 0(zero)
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0)
                 q.add(i);
         }
+        int idx=0;
+        //handle edge case
         if(q.isEmpty())
             return new int[0];
-        int idx=0;
         
         while(!q.isEmpty()){
-            int node=q.poll();
-            ans[idx++]=node;
-            for(int it:graph.get(node)){
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.add(it);
+            int x=q.poll();
+            topo[idx++]=x;
+            for(int i:graph.get(x)){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.add(i);
+                }
             }
         }
         if(idx!=numCourses)
             return new int[0];
-       return ans; 
+        
+        return topo;
+        
     }
 }
