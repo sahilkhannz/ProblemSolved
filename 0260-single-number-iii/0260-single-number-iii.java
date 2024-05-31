563b0f85a4bde51f2ac2555a420dc9c6edf9c24d
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-05-31 11:49:25 +0530

    Time: 1 ms (100.00%), Space: 45.4 MB (69.87%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,25 +1,38 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Map<Integer,Integer> freq=new HashMap<>();
+//         Map<Integer,Integer> freq=new HashMap<>();
         
-        for(int i:nums){
-            freq.put(i,freq.getOrDefault(i,0)+1);
-        }
-        boolean flag=true;
-        int ans[]=new int[2];
-        for(int i:nums){
-            if(freq.get(i)==1){
-                if(flag){
-                    ans[0]=i;
-                    flag=false;
-                }else{
-                    ans[1]=i;
-                    break;
-                }
-            }
+//         for(int i:nums){
+//             freq.put(i,freq.getOrDefault(i,0)+1);
+//         }
+//         boolean flag=true;
+//         int ans[]=new int[2];
+//         for(int i:nums){
+//             if(freq.get(i)==1){
+//                 if(flag){
+//                     ans[0]=i;
+//                     flag=false;
+//                 }else{
+//                     ans[1]=i;
+//                     break;
+//                 }
+//             }
             
+//         }
+//         return ans;
+        int xor2no=0;
+        for(int i:nums){
+            xor2no^=i;
         }
-        return ans;
-        
+        int result[]=new int[2];
+        int lowestBit=xor2no & (-xor2no);
+        for(int i:nums){
+            if((lowestBit & i) == 0){
+                result[0]^=i;
+            }else{
+                result[1]^=i;
+            }
+        }
+        return result;
     }
 }
