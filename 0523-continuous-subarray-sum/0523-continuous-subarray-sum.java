6014606feac59585efa273f9e29396188feced40
sahilkhannz <56390773+sahilkhannz@users.noreply.github.com>
2024-06-08 19:41:59 +0530

    Time: 23 ms (56.12%), Space: 57.9 MB (50.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,14 +1,18 @@
 class Solution {
+    //modulo ki jay ho
     public boolean checkSubarraySum(int[] nums, int k) {
         int n=nums.length;
         if(n<2) return false;
-        
+        //we store mod of all of sum of array
+        //if we do mod of sum of array if we get 2 times same mod that means
+        //it's multiple of k
         Map<Integer,Integer> map=new HashMap<>();
         map.put(0,-1);
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
             int mod=sum%k;
+            //check we seen it before?
             if(map.containsKey(mod)){
                 int l=i-map.get(mod);
                 if(l>=2)
